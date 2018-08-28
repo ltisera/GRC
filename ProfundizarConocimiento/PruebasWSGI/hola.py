4f1abc6378b16880e77e96199d478e5b8f7e34cc
reypala <lucastisera@gmail.com>
2018-08-28 16:51:28 -0300

    Test Modelo de DAO

--- ProfundizarConocimiento/PruebasWSGI/hola.py
+++ ProfundizarConocimiento/PruebasWSGI/hola.py
@@ -1,9 +1,11 @@
 from wsgiref.simple_server import make_server
 import mysql.connector
 
+from UsuarioDAO import UsuarioDAO
+
+
 def hello_world(environ, start_response):
     status = '200 ok'
-
     print("levantando resp_archivo")
     resp_archivo = open('hola.htm', "r")
     print("levantado")
@@ -13,30 +15,9 @@ def hello_world(environ, start_response):
 
 
 def run():
-
-    print("Levantando BD")
-    bd = mysql.connector.connect(
-        host="localhost",
-        user="adminsuve",
-        passwd="1234suve",
-        database="usuario")
-    micur = bd.cursor()
-    micur.execute("SHOW COLUMNS FROM usuario")
-    print("Los campos")
-    for i in micur:
-        print(i[0])
-    micur.execute("SELECT * FROM usuario")
-    print("Fin de los campos\n")
-    for i in micur:
-        print("Iteracion")
-
-        print(i)
-    print("insertando datos:")
-    for n in range(10):
-        nombre = "pepe" + str(n)
-        print (nombre)
-        micur.execute("INSERT INTO usuario(ussr,passwd,mail) VALUES ('{0}','123','buena')".format(nombre))
-        bd.commit()
+    udao = UsuarioDAO()
+    elusr = udao.traerUsuario(0)
+    print(elusr)
     httpd = make_server('', 8006, hello_world)
     print('Serving on port 8006...')
     httpd.serve_forever()
--- ProfundizarConocimiento/PruebasWSGI/usuario.py
+++ ProfundizarConocimiento/PruebasWSGI/usuario.py
@@ -1,5 +1,70 @@
 
 
 
+
+
+
+
+
+
+class Usuario():
+    def __init__(self, nombre="", apellido="", email="", password=""):
+        self._idUsuario = None
+        self._nombre = nombre
+        self._apellido = apellido
+        self._email = email
+        self._password = password
+        
+
+    @property
+    def idUsuario(self):
+        return self._idUsuario
+
+    @idUsuario.setter
+    def idUsuario(self, id):
+        self._idUsuario = id
+
+    @property
+    def nombre(self):
+        return self._nombre
+
+    @nombre.setter
+    def nombre(self, nombre):
+        self._nombre = nombre
+
+    @property
+    def apellido(self):
+        return self._apellido
+
+    @apellido.setter
+    def apellido(self, apellido):
+        self._apellido = apellido
+
+    @property
+    def email(self):
+        return self._email
+
+    @email.setter
+    def email(self, email):
+        self._email = email
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, password):
+        self._password = password
+
+    def aCadena(self):
+        return ("id: " + str(self.idUsuario) +
+                " Nombre: " + self.nombre + " " +
+                self.apellido + "\nE-mail: " +
+                self.email + " Password: "
+                + self.password)
+
 if __name__ == '__main__':
+    usrtst = Usuario
+    usrtst.nombre = "Nico"
+    print(usrtst.nombre)
     print("Estoy en el main del ussr")
