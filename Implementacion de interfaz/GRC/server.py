0e7d4fdc84fc07c54fc2525de9a789bb803e430f
cmathov <31076435+cmathov@users.noreply.github.com>
2018-10-05 19:39:42 -0300

    Index con login responsive
    La imagen de fondo es de muestra, puede ser cualquiera que funcione en modo mosaico.

--- Implementacion de interfaz/GRC/server.py
+++ Implementacion de interfaz/GRC/server.py
@@ -2,12 +2,8 @@ from flask import Flask, render_template, send_from_directory, request, jsonify
 from UsuarioDAO import UsuarioDAO
 import json
 
-app = Flask(__name__)
 
-
-@app.route('/')
-def inicio():
-    return('<p>Estas en la raiz</p>')
+app = Flask(__name__, static_folder='static', static_url_path='')
 
 
 @app.route('/js/jquery-3.3.1.js')
@@ -15,9 +11,34 @@ def jsfile():
     return send_from_directory('static/js', 'jquery-3.3.1.js')
 
 
-@app.route('/index')
+@app.route('/js/bootstrap.min.js')
+def jsbootstrapfile():
+    return send_from_directory('static/js', 'bootstrap.min.js')
+
+
+@app.route('/css/bootstrap.min.css')
+def cssbootstrapfile():
+    return send_from_directory('static/css', 'bootstrap.min.css')
+
+
+@app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index2.html')
+
+
+@app.route('/header1')
+def header1():
+    return render_template('header1.html')
+
+
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
+
+
+@app.route('/info')
+def info():
+    return render_template('info.html')
 
 
 @app.route('/miajax', methods=['GET', 'POST'])
